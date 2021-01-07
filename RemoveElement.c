9141208db01eaebe3b445ac9e1a35da3e25191b3
Vishal Bharadwaj <47346942+vbadwaj@users.noreply.github.com>
2021-01-07 18:50:47 +0400

    Create RemoveElement.c

---
+++ RemoveElement.c
@@ -0,0 +1,12 @@
+
+
+int removeElement(int* nums, int numsSize, int val){
+    int i=0,j=0;
+    for(j=0;j<numsSize;j++){
+        if(nums[j]!=val){
+            nums[i++]=nums[j];
+        }
+    }
+    return i;
+
+}
